d2063134b0a0d18af363b0d33d65f67161d243fa
BingYou Leong <byleong1990@gmail.com>
2023-06-20 16:00:09 +0800

    added leaderboard and fixed minor lint issues

--- quiz-client/src/components/Leaderboard.js
+++ quiz-client/src/components/Leaderboard.js
@@ -1,132 +1,69 @@
+import React, { useEffect, useState } from "react";
+// import { Table } from "reactable";
+import { createAPIEndpoint, ENDPOINTS } from "../api";
+// import { useStateContext } from "../hooks/useStateContext";
 import {
-  Alert,
-  Button,
+  Box,
   Card,
   CardContent,
-  CardMedia,
   Typography,
 } from "@mui/material";
-import { Box } from "@mui/system";
-import React, { useEffect, useState } from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+// import Paper from '@mui/material/Paper';
+/* import { Box } from "@mui/system";
 import { useNavigate } from "react-router";
-import { createAPIEndpoint, ENDPOINTS } from "../api";
 import { getFormatedTime } from "../helper";
-import { useStateContext } from "../hooks/useStateContext";
 import { green } from "@mui/material/colors";
-import Answer from "./Answer";
+ */
 
-export default function Result() {
-  const { context, setContext } = useStateContext();
-  const [score, setScore] = useState(0);
-  const [qnAnswers, setQnAnswers] = useState([]);
-  const [showAlert, setShowAlert] = useState(false);
-  const navigate = useNavigate();
+export default function Leaderboard() {
+  // const { context } = useStateContext();
+  const [participants, setParticipants] = useState([]);
 
   useEffect(() => {
-    const ids = context.selectedOptions.map((x) => x.qnId);
-    createAPIEndpoint(ENDPOINTS.getAnswers)
-      .post(ids)
+    createAPIEndpoint(ENDPOINTS.leaderboard)
+      .fetch()
       .then((res) => {
-        const qna = context.selectedOptions.map((x) => ({
-          ...x,
-          ...res.data.find((y) => y.qnId == x.qnId),
-        }));
-        setQnAnswers(qna);
-        calculateScore(qna);
+        setParticipants(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
 
-  const calculateScore = (qna) => {
-    let tempScore = qna.reduce((acc, curr) => {
-      return curr.answer == curr.selected ? acc + 1 : acc;
-    }, 0);
-    setScore(tempScore);
-  };
-
-  const restart = () => {
-    setContext({
-      timeTaken: 0,
-      selectedOptions: [],
-    });
-    navigate("/quiz");
-  };
-
-  const submitScore = () => {
-    createAPIEndpoint(ENDPOINTS.participant)
-      .put(context.participantId, {
-        participantId: context.participantId,
-        score: score,
-        timeTaken: context.timeTaken,
-      })
-      .then((res) => {
-        setShowAlert(true);
-        setTimeout(() => {
-          setShowAlert(false);
-        }, 4000);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
   return (
-    <>
-      <Card
-        sx={{
-          mt: 5,
-          display: "flex",
-          width: "100%",
-          maxWidth: 640,
-          mx: "auto",
-        }}
-      >
-        <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
-          <CardContent sx={{ flex: "1 0 auto", textAlign: "center" }}>
-            <Typography variant="h4">Congratulations!</Typography>
-            <Typography variant="h6">YOUR SCORE</Typography>
-            <Typography variant="h5" sx={{ fontWeight: 600 }}>
-              <Typography variant="span" color={green[500]}>
-                {score}
-              </Typography>
-              /5
-            </Typography>
-            <Typography variant="h6">
-              {/* Took {getFormatedTime(context.timeTaken) + " mins"} */}
-              You took {getFormatedTime(context.timeTaken)}
-            </Typography>
-            <Button
-              variant="contained"
-              sx={{ mx: 1 }}
-              size="small"
-              onClick={submitScore}
-            >
-              Submit
-            </Button>
-            <Button
-              variant="contained"
-              sx={{ mx: 1 }}
-              size="small"
-              onClick={restart}
-            >
-              Re-try
-            </Button>
-            <Alert
-              severity="success"
-              variant="string"
-              sx={{
-                width: "60%",
-                m: "auto",
-                visibility: showAlert ? "visible" : "hidden",
-              }}
-            >
-              Score Updated.
-            </Alert>
-          </CardContent>
+    <Card sx={{ width: '75%', m: "auto" }}>
+      <CardContent>
+        <Box sx={{ display: "flex", justifyContent: "center"}}>
+          <Typography variant="h4" sx={{ mb: 2 }}>
+            Leaderboard
+          </Typography>
         </Box>
-        <CardMedia component="img" sx={{ width: 220 }} image="./result.png" />
-      </Card>
-      <Answer qnAnswers={qnAnswers} />
-    </>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell align="right">Score</TableCell>
+                <TableCell align="right">Time Taken</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {participants.map((participant) => (
+                <TableRow key={participant.name}>
+                {/* <TableRow> */}
+                  <TableCell>{participant.name}</TableCell>
+                  <TableCell align="right">{participant.score}</TableCell>
+                  <TableCell align="right">{participant.timeTaken}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </CardContent>
+    </Card>
   );
 }
