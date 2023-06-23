b147d723b5a80e0493259e264d97a8ceaa8fe245
BingYou Leong <byleong1990@gmail.com>
2023-06-23 15:57:45 +0800

    added sql question injection

---
+++ quiz-client/SQLQuery.sql
@@ -0,0 +1,30 @@
+USE [moviequizdb]
+GO
+
+SET IDENTITY_INSERT [dbo].[Questions] ON 
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (1, N'To crush your enemies, see them driven before you, and to hear the lamentations of their women.', NULL, N'Triumph of the Spirit (1989)', N'Conan the Barbarian(1982)', N'First Blood (1982)', N'Missing in Action (1984)', 1)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (2, N'Stay in school and use your brain. Be a doctor, be a lawyer, carry a leather briefcase.', NULL, N'Taxi Driver (1976)', N'The Joy Luck Club (1993)', N'Rocky (1976)', N'Good Will Hunting (1997)', 2)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (3, N'Out of the kindness of his heart he gave you a job with his family. Contrary to my advice you took the job. I offered you something better. Something in the legitimate world.', NULL, N'Goodfellas (1990)', N'Pulp Fiction (1994)', N'The Godfather Part III (1990)', N'Heat (1995)', 2)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (4, N'You can''t handle the truth!', NULL, N'A Few Good Men (1992)', N'Born on the Fourth of July (1989)', N'The Crossing Guard (1995)', N'Top Gun (1986)', 0)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (5, N'My mama always said life was like a box of chocolates. You never know what you''re gonna get.', NULL, N'The Green Mile (1999)', N'Forrest Gump (1994)', N'Sleepless in Seattle (1993)', N'Saving Private Ryan (1998)', 1)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (6, N'See, the sad thing about a guy like you is, in 50 years you''re gonna start doin'' some thinkin'' on your own and you''re going to come up with the fact that there are two certainties in life.', NULL, N'Inventing the Abbotts (1997)', N'What''s Eating Gilbert Grape (1993)', N'Boyz in the Hood (1991)', N'Good Will Hunting (1997)', 3)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (7, N'Sorry for the interruption, folks, but I always do the last dance of the season. This year somebody told me not to.', NULL, N'Dirty Dancing (1987)', N'Grease (1978)', N'Saturday Night Fever (1977)', N'Step Up (2006)', 0)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (8, N'Yeah. I hope you boys have learned as much from me this year as I''ve learned from you. You''ve taught this city how to trust the soul of a man rather than the look of him.', NULL, N'Rudy (1993)', N'Remember the Titans (2000)', N'Friday Night Lights (2004)', N'Varsity Blues (1999)', 1)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (9, N'You either die a hero or you live long enough to see yourself become the villain.', NULL, N'Hellboy (2004)', N'Batman Begins (2005)', N'V for Vendetta (2005)', N'The Dark Knight (2008)', 3)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (10, N'Every man dies, not every man really lives.', NULL, N'Goldeneye (1995)', N'Braveheart (1995)', N'Die Hard with a Vengeance (1995)', N'Bad Boys (1995)', 1)
+GO
+SET IDENTITY_INSERT [dbo].[Questions] OFF
+
+GO
+
+
